19c9435a04496dcdcd880c451c7e00d0afcea046
taeguk <xornrbboy@gmail.com>
2016-06-09 13:02:34 -0400

    v0.0.3

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup (
     name = 'github-export',
-    version = '0.0.2',
+    version = '0.0.3',
 
     author = 'taeguk',
     author_email = 'xornrbboy@gmail.com',
